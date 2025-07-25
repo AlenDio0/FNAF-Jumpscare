2ef79771c38943fae0b13c89577ce2d600e207d6
AlenDio0 <alendio.0000@gmail.com>
2025-07-26 01:54:43 +0200

    Fixed mod frames and preview

--- Source/FNAF Jumpscare/JumpscareWindow.cs
+++ Source/FNAF Jumpscare/JumpscareWindow.cs
@@ -11,8 +11,9 @@ namespace FNAFJumpscare
         private int m_CurrentFrame = 0;
         private float m_LastFrameTime = Time.realtimeSinceStartup;
 
-        private const float m_FrameDuration = 0.02f;
-        private const int m_FramesCount = 26;
+        private const float m_FrameDuration = 0.03f;
+        private const float m_LastFrameDuration = 0.5f;
+        private const int m_FramesCount = 13;
 
         public override Vector2 InitialSize => new Vector2(UI.screenWidth, UI.screenHeight);
 
@@ -25,8 +26,8 @@ namespace FNAFJumpscare
             absorbInputAroundWindow = true;
             forcePause = true;
 
-            for (int i = 0; i < m_FramesCount; i++)
-                m_Frames.Add(ContentFinder<Texture2D>.Get($"Jumpscare/Foxy_{i:D8}"));
+            for (int i = 1; i <= m_FramesCount; i++)
+                m_Frames.Add(ContentFinder<Texture2D>.Get($"Jumpscare/Foxy_{i:D2}"));
 
             SoundDef sound = SoundDef.Named("FNAFScreamer");
             sound.PlayOneShotOnCamera();
@@ -34,15 +35,20 @@ namespace FNAFJumpscare
 
         public override void DoWindowContents(Rect canva)
         {
-            if (m_CurrentFrame >= m_Frames.Count)
+            float currentTime = Time.realtimeSinceStartup;
+
+            m_CurrentFrame = Mathf.Clamp(m_CurrentFrame, 0, m_Frames.Count - 1);
+            GUI.DrawTexture(canva, m_Frames[m_CurrentFrame], ScaleMode.ScaleToFit, true);
+
+            if (m_CurrentFrame == m_Frames.Count - 1)
             {
-                Close();
+                if (currentTime - m_LastFrameTime < m_LastFrameDuration)
+                    return;
+
+                Close(false);
                 return;
             }
 
-            GUI.DrawTexture(canva, m_Frames[m_CurrentFrame], ScaleMode.ScaleToFit, true);
-
-            float currentTime = Time.realtimeSinceStartup;
             if (currentTime - m_LastFrameTime < m_FrameDuration)
                 return;
 
